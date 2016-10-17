a04b5daf6b9fe69c8c9ee6b186f89cd7ab34ec04
Ondrej Zdych <zdych.ondrej@gmail.com>
2016-10-17 12:37:47 +0200

    feat: initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const cache = require('gulp-cache');
+const { execSync } = require('child_process');
+
+const eslintCache = new cache.Cache({ cacheDirName: 'eslint-cache' });
+
+const eslintVersion = 'dependencies.gulp-eslint.dependencies.eslint.version'
+	.split('.')
+	.reduce((acc, curr) => acc[curr], JSON.parse(execSync('npm ls eslint --json').toString()));
+
+function makeEslintCacheKey (file) {
+	return [ file.contents.toString('utf8'), fs.readFileSync('.eslintrc'), eslintVersion ].join('');
+}
+
+gulp.task('clean', (done) => {
+	eslintCache.clear(undefined, done);
+});
+
+gulp.task('lint', () => {
+	return gulp.src('./src/*.js')
+		.pipe(cache(eslint(), {
+			key: makeEslintCacheKey,
+			fileCache: eslintCache,
+			success: ({ eslint: { errorCount, warningCount } }) => errorCount + warningCount === 0,
+		}))
+		.pipe(eslint.format());
+});
+
+gulp.task('watch', [ 'lint' ], () => gulp.watch('./src/*.js', [ 'lint' ]));
+
+gulp.task('default', [ 'watch' ]);
